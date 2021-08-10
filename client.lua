6eb0fbc4628fcb1a0af7320cf67baf9e4e7771a7
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-08-10 18:12:46 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -8,3 +8,28 @@ AddEventHandler('player:loadcharacterclothes', function(source, charappearance)
   exports["fivem-appearance"]:setPlayerAppearance(appearance)
   print('Loaded clothes')
 end)
+
+-- Command provided with default resource but with a server event trigger to 
+-- save the outfit to the kvs database
+
+RegisterCommand('skin', function()
+    local config = {
+      ped = true,
+      headBlend = true,
+      faceFeatures = true,
+      headOverlays = true,
+      components = true,
+      props = true,
+    }
+
+    local source = source
+    local playerid = GetPlayerFromServerId(source)
+    exports['fivem-appearance']:startPlayerCustomization(function (appearance)
+        if (appearance) then
+        print('Saved')        
+        TriggerServerEvent('updatecharacterclothes', appearance)
+        else
+        print('Canceled')
+        end
+    end, config)
+  end, false)
